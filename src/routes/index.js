a5d8ba50946d0c19a03ba2d0d9dff6eb36d3b235
KevinJoseph <kevramos95@gmail.com>
2020-03-28 17:30:45 -0500

    new

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,6 @@ const router = require('express').Router();
 
 
 router.get('/', (req,res) => {
-    res.render('index2');
-});
-router.get('/maps', (req,res) => {
     res.render('index');
 });
 module.exports = router;
